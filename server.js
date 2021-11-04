e6fe7c253b5ea31319333505f35c07b1480def6f
ashucross <45733553+ashucross@users.noreply.github.com>
2021-11-04 23:41:30 +0530

    node live fix

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(
         extended: false
     })
 );
-const port = 8080;
+const port = 5000;
 app.use(bodyParser.json());
-/* app.use('/',express.static(path.join(__dirname, './client/build'))); */
+/* app.use('/',express.static(path.join(__dirname, './build')));  */
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
@@ -31,10 +31,10 @@ mongoose
     app.use("/api/users", users);
 /* const port = process.env.PORT || 8080; // process.env.port is Heroku's port if you choose to deploy the app there
  */
-/* app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-}); */
+/*   app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'));
+});  */
 
 
-app.listen(port,'https://v1.kuuria.com', () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
