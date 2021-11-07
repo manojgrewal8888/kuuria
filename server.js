974e0d62e1ec2a96bb6b47b113e04e508e0afad0
ashucross <45733553+ashucross@users.noreply.github.com>
2021-11-07 23:01:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const app = express();
 const passport = require("passport");
 const users = require("./routes/api/users");
 // Bodyparser middleware
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
-const port = 5000;
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 /* app.use('/',express.static(path.join(__dirname, './build')));  */
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-    .connect(
-        db, { useNewUrlParser: false }
+.connect(
+    db, { useNewUrlParser: false }
     )
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
@@ -29,6 +24,7 @@ mongoose
     require("./config/passport")(passport);
     // Routes
     app.use("/api/users", users);
+    const port = 5000;
 /* const port = process.env.PORT || 8080; // process.env.port is Heroku's port if you choose to deploy the app there
  */
 /*   app.get('/', (req,res) => {
