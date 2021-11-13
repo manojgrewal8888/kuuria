734bf4f07ceb34a4a5dafc682099c4d7a3eaedcd
ashucross <45733553+ashucross@users.noreply.github.com>
2021-11-13 19:02:43 +0530

    Sidebar

--- client/src/components/front/Sidebar.js
+++ client/src/components/front/Sidebar.js
@@ -19,16 +19,20 @@ class Sidebar extends Component {
                     {/* <div className="home_vendor_">
                         <span className="home_vender2"><Link className="linx_ven" to="/vendor_dash"> <i className="fa fa-home"></i>  Dashboard </Link> </span> 
                         </div>  */}
-                    <Link className="linx_ven" exact to='/vendor_dashboard'> <i className="fa fa-home cus_vicon"></i></Link> 
+                        <Link className="" exact to='/vendor_dashboard'>
+                        <div class='custom_btn1'>
+                            <i className="fa fa-home cus_vicon"></i>
+                              <span className='menu_it_em'>Dashboard</span>
+                        </div>
+                        </Link> 
                     <Link className="linx_ven" exact to='/manage_events'> <i className="fa fa-calendar-check-o cus_vicon"></i></Link> 
-                    <div className="home_vendori3">
-                        <button className="custom_btn">
+
+                    {/* <div className="home_vendori3">
+                        <button className="">
                         <i className="fa fa-ticket cus_vicon3"></i>
                        MANAGE TICKET
-                        </button>
-                   
-              
-                    </div>
+                        </button> 
+                    </div> */}
 
                     <Link className="linx_ven" exact to="/"><i className="fa fa-money cus_vicon"></i></Link>
                     <Link className="linx_ven" exact to="/"><i className="fa fa-comment cus_vicon"></i></Link>
