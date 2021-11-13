da77172de78e59583453ed9c865a91be7a7ab102
shadowcodex12 <92997859+shadowcodex12@users.noreply.github.com>
2021-11-13 19:22:42 +0530

    head

--- client/src/components/front/Sidebar.js
+++ client/src/components/front/Sidebar.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions"; 
+import { logoutUser } from "../../actions/authActions";
 import { Link } from "react-router-dom";
 class Sidebar extends Component {
     onLogoutClick = e => {
         e.preventDefault();
         this.props.logoutUser(this.props.history);
-      };
+    };
     render() {
         const { user } = this.props.auth;
         return (
-                <>
+            <>
                 <div className="left_vendor">
                     <div className="left_subven">
-                    <Link className="linx_ven" exact to="/"><img src="./img/kuria-new.png" alt="" className="res_venlogo" /></Link>
-                    {/* <div className="home_vendor_">
+
+                    <div className="home_vendori3">  
+                        <Link className="linx_ven" exact to="/"><img src="./img/kuria-new.png" alt="" className="res_venlogo" /></Link>
+                    </div>
+                        {/* <div className="home_vendor_">
                         <span className="home_vender2"><Link className="linx_ven" to="/vendor_dash"> <i className="fa fa-home"></i>  Dashboard </Link> </span> 
                         </div>  */}
                         <Link className="" exact to='/vendor_dashboard'>
@@ -34,10 +37,6 @@ class Sidebar extends Component {
                         </button> 
                     </div> */}
 
-                    <Link className="linx_ven" exact to="/"><i className="fa fa-money cus_vicon"></i></Link>
-                    <Link className="linx_ven" exact to="/"><i className="fa fa-comment cus_vicon"></i></Link>
-                    <Link className="linx_ven" exact to="/"><i className="fa fa-bar-chart cus_vicon"></i></Link>
-                    <Link className="linx_ven" exact to="/"><i className="fa fa-file-text-o cus_vicon"></i></Link>
                         <button
                             style={{
                                 width: "150px",
@@ -47,23 +46,23 @@ class Sidebar extends Component {
                             }}
                             onClick={this.onLogoutClick}
                             className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >Logout &nbsp;
-                            <i className="fa fa-sign-out"></i> 
-                        </button> 
+                        >Logout &nbsp;
+                            <i className="fa fa-sign-out"></i>
+                        </button>
                     </div>
                 </div>
-                </>
+            </>
         )
     }
 }
 Sidebar.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
-    };
-    const mapStateToProps = state => ({
+};
+const mapStateToProps = state => ({
     auth: state.auth
-    });
-    export default connect(
+});
+export default connect(
     mapStateToProps,
     { logoutUser }
 )(Sidebar);
