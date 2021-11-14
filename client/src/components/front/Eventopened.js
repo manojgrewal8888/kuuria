b2e8b91ee61efbd4845e7e958b70cb18a4af3386
shadowcodex12 <92997859+shadowcodex12@users.noreply.github.com>
2021-11-14 16:58:54 +0530

    new

--- client/src/components/front/Eventopened.js
+++ client/src/components/front/Eventopened.js
@@ -1,86 +1,79 @@
 import React, { PureComponent } from 'react';
-import { PieChart, Pie,Cell } from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
 import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const data = [
-  { name: 'Group A', value: 900 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  
+    { name: 'Group A', value: 900 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+
 ];
 const COLORS = ['#ffffff', '#008cff', '#FFd600'];
 
 export default class Opened extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
+    static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
 
-  render() {
-    return (
-    <div>
+    render() {
+        return (
+            <div>
 
-        <div className="contain_home">
-            <div className="left_home">
-                <img src="./img/kuria-new.png" alt="" className="event_img" />
-                <Link className="link_reset" to='/eventopened'><i className="fa fa-home home_icon home_iconsm"></i><span className="only_edash">Dashboard</span></Link>
-                <Link className="link_reset" to='/settinggen'><i className="fa fa-gear home_icon"></i></Link>
-                <Link className="link_reset" to='/category'><i className="fa fa-th home_icon"></i></Link>
-                <Link className="link_reset" to='/category'><i className="fa fa-trash home_icon"></i></Link>
-                
+                <div className="wrap_vwendor">
+                    <Sidebar history={this.props.history} />
+
+
+
+                    <div className="right_home">
+
+                        <div className="open_lin1"></div>
+                        <div className="open_lin2"></div>
+
+                        <div className="date_format">
+                            <div className="devide_opened"></div>
+
+                            <i className="fa fa-calendar-o cal_left"></i>
+                            <span className="date_o1">Start Date</span>
+                            <span className="date_o2">September 27th,2021</span>
+                            <span className="date_o3">10:00 AM</span>
+
+                            <i className="fa fa-calendar-o cal_right"></i>
+                            <span className="date_o4">End Date</span>
+                            <span className="date_o5">October 27th,2021</span>
+                            <span className="date_o6">10:00 AM</span>
+
+                        </div>
+
+                        <input className="input_opened" type="text" placeholder="//http karmatech.com" />
+
+                       
+
+                        <PieChart width={325} height={500} onMouseEnter={this.onPieEnter}>
+                            <Pie
+                                data={data}
+                                cx={150}
+                                cy={200}
+                                innerRadius={120}
+                                outerRadius={150}
+                                fill="#8884d8"
+                                paddingAngle={5}
+                                dataKey="value"
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                        </PieChart>
+
+                     <div className="spans_ope">
+                         <div className="one_op1"><span className="open_spv"><i className="fa fa-circle white_cop"></i>Votes</span> </div>
+                         <div className="one_op2"><span className="open_spv2"><i className="fa fa-circle blue_cop"></i>Nominee</span> </div>
+                         <div className="one_op3"><span className="open_spv3"><i className="fa fa-circle yellow_cop"></i>Category</span> </div>
+                     </div>
+
+
+                    </div>
+                </div>
             </div>
-                    
-            <div className="right_home">
-
-            <div className="open_lin1"></div>
-            <div className="open_lin2"></div>
-
-            <div className="date_format">
-                <div className="devide_opened"></div>
-
-                <i className="fa fa-calendar-o cal_left"></i>
-                <span className="date_o1">Start Date</span>
-                <span className="date_o2">September 27th,2021</span>
-                <span className="date_o3">10:00 AM</span>
-                
-                <i className="fa fa-calendar-o cal_right"></i>
-                <span className="date_o4">End Date</span>
-                <span className="date_o5">October 27th,2021</span>
-                <span className="date_o6">10:00 AM</span>
-
-            </div>
-
-            <input className="input_opened" type="text" placeholder="//http karmatech.com" />
-
-            <i className="fa fa-circle cwhite"></i>
-                <span className="vot_count">Voter</span>
-
-                <i className="fa fa-circle cblue"></i>
-                <span className="nomi_count">Nominee</span>
-
-                <i className="fa fa-circle cyellow"></i>
-                <span className="categories_count">Category</span>
-
-                <PieChart width={400} height={700} onMouseEnter={this.onPieEnter}>
-                    <Pie
-                    data={data}
-                    cx={150}
-                    cy={480}
-                    innerRadius={120}
-                    outerRadius={150}
-                    fill="#8884d8"
-                    paddingAngle={5}
-                    dataKey="value"
-                    >
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                    </Pie>
-                </PieChart>
-
-                <span className="count_x1">400</span><span className="count_x2">70</span><span className="count_x3">120</span>
-
-
-            </div>
-        </div>
-    </div>
-    );
-  }
+        );
+    }
 }
