04e7ef1345d07775b985317512323e42b8e1e02d
shadowcodex12 <92997859+shadowcodex12@users.noreply.github.com>
2021-12-10 15:13:11 +0530

    newcss

--- client/src/components/front/Menu.js
+++ client/src/components/front/Menu.js
@@ -3,26 +3,26 @@ import { NavLink } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 import PropTypes from "prop-types";
-import { connect } from "react-redux";  
+import { connect } from "react-redux";
 import axios from "axios";
 export default class Menu extends Component {
     constructor() {
         super();
-        this.state = { 
+        this.state = {
             showDashboard: false
         };
     }
     componentWillReceiveProps(nextProps) {
 
     }
-    componentDidMount() {   
-        var user_id = localStorage.getItem('_id'); 
-        if(user_id){
+    componentDidMount() {
+        var user_id = localStorage.getItem('_id');
+        if (user_id) {
             this.setState({
                 showDashboard: true
             });
         }
-        
+
     }
 
 
@@ -39,21 +39,21 @@ export default class Menu extends Component {
                     <div className="menu">
 
                         <div className="dropmenu_mai">
-                        <div className="dropdownzx">
-                            <button className="dropbtnzx">MENU</button>
-                            <div className="dropdown-contentzx">
-                            <Link className="link_resetmen"  to='/ticket'>Ticketing</Link>
-                            <Link className="link_resetmen"  to='/dashvote'>File Nomination</Link>
-                            {!this.state.showDashboard && <>
-                            <Link className="link_resetmen"  to='/signup'>Sign Up</Link>
-                            <Link className="link_resetmen"  to='/login'>Log In</Link>
-                            </> }
-                            {this.state.showDashboard && this.state.showDashboard==true && <>
-                            <Link className="link_resetmen"  to='/dashboard'>Dashboard</Link> 
-                            </> }
-                            <Link className="link_resetmen"  to='/license'>Buy Licence</Link>
+                            <div className="dropdownzx">
+                                <button className="dropbtnzx"><i className="fa fa-align-justify custopm"></i></button>
+                                <div className="dropdown-contentzx">
+                                    <Link className="link_resetmen" to='/ticket'>Ticketing</Link>
+                                    <Link className="link_resetmen" to='/dashvote'>File Nomination</Link>
+                                    {!this.state.showDashboard && <>
+                                        <Link className="link_resetmen" to='/signup'>Sign Up</Link>
+                                        <Link className="link_resetmen" to='/login'>Log In</Link>
+                                    </>}
+                                    {this.state.showDashboard && this.state.showDashboard == true && <>
+                                        <Link className="link_resetmen" to='/dashboard'>Dashboard</Link>
+                                    </>}
+                                    <Link className="link_resetmen" to='/license'>Buy Licence</Link>
+                                </div>
                             </div>
-                        </div>
 
                         </div>
 
@@ -66,17 +66,17 @@ export default class Menu extends Component {
                                 <NavLink exact={true} activeClassName="active-class" className="navitem" to="/dashvote">File Nomination</NavLink>
                             </li>
                             {!this.state.showDashboard && <>
-                            <li className="navli" >
-                                <NavLink exact={true} activeClassName="active-class" className="navitem" to="/signup">Sign Up</NavLink>
-                            </li>
-                            <li className="navli" >
-                                <NavLink exact={true} activeClassName="active-class" className="navitem" to="/login">Log In</NavLink>
-                            </li>
+                                <li className="navli" >
+                                    <NavLink exact={true} activeClassName="active-class" className="navitem" to="/signup">Sign Up</NavLink>
+                                </li>
+                                <li className="navli" >
+                                    <NavLink exact={true} activeClassName="active-class" className="navitem" to="/login">Log In</NavLink>
+                                </li>
                             </>}
-                            {this.state.showDashboard && this.state.showDashboard==true && <>
-                            <li className="navli" >
-                                <NavLink exact={true} activeClassName="active-class" className="navitem" to="/dashboard">Dashboard</NavLink>
-                            </li>
+                            {this.state.showDashboard && this.state.showDashboard == true && <>
+                                <li className="navli" >
+                                    <NavLink exact={true} activeClassName="active-class" className="navitem" to="/dashboard">Dashboard</NavLink>
+                                </li>
                             </>}
                             <li className="navli" >
                                 <NavLink exact={true} activeClassName="active-class" className="navitem" to="/license">Buy License</NavLink>
